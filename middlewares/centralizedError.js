7793982c2ccef35ef1553a2f1ba07123e514784b
Chad Casey <gbchadd@gmail.com>
2024-04-11 21:02:02 -0700

    typos

--- middlewares/centralizedError.js
+++ middlewares/centralizedError.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
   console.log(err);
   const { message, status = 500 } = err;
 
-  res.stauts(err.status).send({
+  res.status(err.status).send({
     message: status === 500 ? "An error occured on the server" : err.message,
   });
 };
